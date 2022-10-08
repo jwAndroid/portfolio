76eeb63f6bfed1aca75bb4e6bc6443745790a605
jwAndroid <cjd9408er@naver.com>
2022-10-08 19:12:00 +0900

    Porg project

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+// TODO: Project
 const root = ReactDOM.createRoot(
   document.getElementById('root') as Element | DocumentFragment
 );
