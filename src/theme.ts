c4bd17016ca949a05248c938f373a663f262c31f
jwAndroid <cjd9408er@naver.com>
2022-10-13 20:15:20 +0900

    del image

--- src/theme.ts
+++ src/theme.ts
@@ -1,32 +1,7 @@
 import { Theme } from '@emotion/react';
 
 export const image = {
-  android: require('../src/assets/image/android.png'),
-  chatmo: require('../src/assets/image/chatmo.png'),
-  coffee: require('../src/assets/image/coffee.png'),
-  covid: require('../src/assets/image/covid.png'),
-  dart: require('../src/assets/image/dart.png'),
-  emotion: require('../src/assets/image/emotion.png'),
-  eslint: require('../src/assets/image/eslint.png'),
-  expo_background: require('../src/assets/image/expo_background.png'),
-  expo: require('../src/assets/image/expo.png'),
-  firebase: require('../src/assets/image/firebase.png'),
-  friending: require('../src/assets/image/friending.png'),
-  github: require('../src/assets/image/github.png'),
-  java: require('../src/assets/image/java.png'),
-  javascript: require('../src/assets/image/javascript.png'),
-  kotlin: require('../src/assets/image/kotlin.png'),
-  noteapp: require('../src/assets/image/noteapp.png'),
   profile: require('../src/assets/image/profile.jpg'),
-  project: require('../src/assets/image/project.jpg'),
-  react_query: require('../src/assets/image/react_query.png'),
-  react: require('../src/assets/image/react.png'),
-  redux_thunk: require('../src/assets/image/redux_thunk.png'),
-  redux: require('../src/assets/image/redux.png'),
-  rx: require('../src/assets/image/rx.png'),
-  todoapp: require('../src/assets/image/todoapp.png'),
-  typescript: require('../src/assets/image/typescript.png'),
-  wetrip: require('../src/assets/image/wetrip.png'),
 };
 
 export const AppTheme: Theme = {
